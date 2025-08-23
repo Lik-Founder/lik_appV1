61b48dbcc9b1ad3ed05fc0899755dee8693dfe89
Lik-Founder <Lik_Reviews@proton.me>
2025-08-23 03:05:30 +0000

    Generated by Spark: Move this to the left. 70 pixels

--- src/components/HomeFeed.tsx
+++ src/components/HomeFeed.tsx
@@ -393,7 +393,7 @@ export function HomeFeed({ onShowUserProfile, onShowRestaurantProfile, onShowLea
             <img 
               src={likLogo} 
               alt="Lik" 
-              className="h-12 w-auto object-contain max-w-[140px] ml-[-20px]"
+              className="h-12 w-auto object-contain max-w-[140px] ml-[-90px]"
             />
           </div>
 
